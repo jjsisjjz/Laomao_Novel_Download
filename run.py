fc9d509b1832dbf4f1103c42078fa97c74971472
Elaina-Alex <64351595+Elaina-Alex@users.noreply.github.com>
2021-10-30 21:19:10 +0800

    修复命令行登入错误

--- run.py
+++ run.py
@@ -3,68 +3,54 @@ import API.Settings
 import sys
 import getopt
 
-
-def shell(argv):
-    ## 开始解析命令行参数
-    choice = 'h'
-    bookid = ''
-    bookname = ''
-    tag = ''
-    pool = True
-    usernames, passwords = None, None
-    try:
-      opts, args = getopt.getopt(argv,"hkc:o:",[
-            "usernames=","passwords=","bookname=",
-            "bookid=","tag=","pool=","max="])
-    except getopt.GetoptError:
-        print('--max <max> pool number')
-        print('--bookid = <id>')
-        print('--bookname = <bookname>')
-        print('--pool  open Thread')
-        print('--usernames # Your Account usernames')
-        print('--passwords')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-help':
-            print('--max <max> pool number')
-            print('--bookid = <id>')
-            print('--bookname = <bookname>')
-            print('--pool  open Thread')
-            print('--usernames # Your Account usernames')
-            print('--passwords')
-            sys.exit()
-        elif opt in ('--pool',):
-            pool = False
-        elif opt in ('--max',):
-            choice = 'settingmax'
-            max = arg
-        elif opt in ('--tagid',):
-            choice = 'gettag'
-            tag = arg
-        elif opt == ('--bookid',):
-            choice = 'bookid dwonload'
-            bookid = arg
-        elif opt in ('--bookname',):
-            choice = 'name dwonload'
-            bookname = arg
-        elif opt in ('--usernames',):
-            usernames = arg
-        elif opt in ('--passwords',):
-            passwords = arg
-        elif opt in ('--rank',):
-            choice = 'ranking'
+choice = 'h'
+bookid = ''
+bookname = ''
+tag = ''
+pool = True
+help = """--max <max> pool number
+--bookid = <id>
+--bookname = <bookname>
+--pool open Thread
+--login = usernames,passwords"""
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"hkc:o:",[
+        "login=","bookname=",
+        "bookid=","tag=","pool=","max="])
+except getopt.GetoptError:
+    print(help)
+    sys.exit(2)
+for opt, arg in opts:
+    if opt in ('--pool',):
+        pool = False
+    elif opt in ('--max',):
+        choice = 'settingmax'
+        max_workers_number = arg
+    elif opt in ('--tagid',):
+        choice = 'gettag'
+        tag = arg
+    elif opt == ('--bookid',):
+        choice = 'bookid dwonload'
+        bookid = arg
+    elif opt in ('--bookname',):
+        choice = 'name dwonload'
+        bookname = arg
+    elif opt in ('--login',):
+        choice = 'login'
+        user = arg
+    elif opt in ('--rank',):
+        choice = 'ranking'
     
-    if usernames is not None and passwords is not None:
-        Download.Login(usernames, passwords)
+def shell():
         
     match choice:
         case 'h':
-            print('--max <max> pool number')
-            print('--bookid = <id>')
-            print('--bookname = <bookname>')
-            print('--pool  open Thread')
-            print('--usernames # Your Account usernames')
-            print('--passwords')
+            print(help)
+
+        case 'login':
+            usernames = user.split(',')[0]
+            passwords = user.split(',')[1]
+            Download.Login(usernames, passwords)
 
         case 'name':
             search_book = Download.SearchBook(bookname)
@@ -100,12 +86,12 @@ def shell(argv):
                     Download.chapters(pool=False)
 
         case 'settingmax':
-            if max.isdigit():
-                Read['max_workers_number'] = 12 if int(max) > 12 else int(max)
+            if max_workers_number.isdigit():
+                Read['max_workers_number'] = 12 if int(max_workers_number) > 12 else int(max_workers_number)
                 print("线程已经设置为", Read['max_workers_number'])
                 Setting.WriteSettings(Read)
             else:
-                print(max, "不是数字，请重新输入")
+                print(max_workers_number, "不是数字，请重新输入")
         case _:
             print("选项为不存在,请输入-h获取帮助")
 
@@ -115,4 +101,4 @@ if __name__ == '__main__':
     Setting = API.Settings.Set()
     Setting.NewSettings()
     Read = Setting.ReadSettings()
-    shell(sys.argv[1:])
+    shell()
